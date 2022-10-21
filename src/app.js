93c848f41d9551b2f70ef9a2e748e61528a3f59b
someone <someone@someplace.com>
2022-10-21 15:08:05 +0700

    some init msg

---
+++ src/app.js
@@ -0,0 +1,13 @@
+var menu = document.querySelector('#mobile-menu')
+var btnMobileMenu = document.querySelector('#btn-mobile-menu')
+var btnMenu = document.querySelector('#btn-menu')
+
+
+btnMobileMenu.onclick = function() {
+    menu.classList.add('hidden')
+}
+
+btnMenu.onclick = function() {
+    menu.classList.remove('hidden')
+    console.log('btnMenu')
+}
